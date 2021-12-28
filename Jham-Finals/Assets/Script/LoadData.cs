449dd48026d115b594778ab03548db9f88ab8ea3
Jhamaica Mae Rosalin <rosalinjhamaica@gmail.com>
2021-12-28 23:25:09 +0800

    jham-finals
    jham-finals

---
+++ Jham-Finals/Assets/Script/LoadData.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LoadData : MonoBehaviour
+{
+    public Text UsernameBox;
+    // Start is called before the first frame update
+    void Start()
+    {
+        UsernameBox.text = PlayerPrefs.GetString("username");
+    }
+    public void clickReset()
+    {
+        PlayerPrefs.DeleteKey("username");
+        SceneManager.LoadScene("firstScene");
+
+    }
+}
